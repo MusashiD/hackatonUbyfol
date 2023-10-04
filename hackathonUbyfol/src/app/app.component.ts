2a8645e437bc3064aaeff8524a45abf09fbddad3
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-10-04 19:27:32 -0300

    progress

--- hackathonUbyfol/src/app/app.component.ts
+++ hackathonUbyfol/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathonUbyfol';
+
+  subscribe(){
+    window.open('https://www.sympla.com.br/evento/hackathon-ubyfol-apoio-fazu-e-parque-tecnologico/2188683?referrer=www.google.com', '_blank');
+  }
 }
