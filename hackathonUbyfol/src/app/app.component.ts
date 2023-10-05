08b684981deaf54203bb34cf81ffecf5cf87fcc0
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-10-05 19:55:35 -0300

    continuacao

--- hackathonUbyfol/src/app/app.component.ts
+++ hackathonUbyfol/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 export class AppComponent {
   title = 'hackathonUbyfol';
   faCheckCircle = faCheckCircle;
+  selectedDiv: string = '27';
   div27: any;
   div28: any;
   div29: any;
@@ -28,6 +29,7 @@ export class AppComponent {
     switch (pin) {
       case '27':
         if (this.div27) {
+          this.selectedDiv = '27'
           if (this.div27.style.display == 'none') {
             this.div27.style.display = 'block';
             this.div28.style.display = 'none';
@@ -40,6 +42,7 @@ export class AppComponent {
 
       case '28':
         if (this.div28) {
+          this.selectedDiv = '28'
           if (this.div28.style.display == 'none') {
             this.div28.style.display = 'block';
             this.div27.style.display = 'none';
@@ -52,6 +55,7 @@ export class AppComponent {
 
       case '29':
         if (this.div29) {
+          this.selectedDiv = '29'
           if (this.div29.style.display == 'none') {
             this.div29.style.display = 'block';
             this.div28.style.display = 'none';
