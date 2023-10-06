a52f9e7c2a514eee184687a33010002b71b83eba
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-10-05 23:20:08 -0300

    scroll suave

---
+++ hackathonUbyfol/src/app/directives/scroll-suave.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ScrollSuaveDirective } from './scroll-suave.directive';
+
+describe('ScrollSuaveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ScrollSuaveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
