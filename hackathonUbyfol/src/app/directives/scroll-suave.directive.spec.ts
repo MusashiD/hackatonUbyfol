ba0954735fe6ffdf0402828496fddc1f008cb0be
Matheus Sizervinks Ferreira <81537258+MathSZVKS@users.noreply.github.com>
2023-10-06 02:16:58 -0300

    Delete hackathonUbyfol/src directory

--- hackathonUbyfol/src/app/directives/scroll-suave.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScrollSuaveDirective } from './scroll-suave.directive';
-
-describe('ScrollSuaveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScrollSuaveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
