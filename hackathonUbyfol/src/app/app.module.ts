97182e616fd884f207f6a3897bf4ecd2239c88e9
Matheus Sizervinks Ferreira <matheussizervinks56@gmail.com>
2023-10-05 22:58:22 -0300

    topbar

--- hackathonUbyfol/src/app/app.module.ts
+++ hackathonUbyfol/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CarouselComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
